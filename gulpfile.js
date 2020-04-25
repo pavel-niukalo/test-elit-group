1f2cc57d38bb6de8e714dcb484ab9ef478223907
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-04-25 21:16:00 +0500

    Начинает мобильную версию

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 "use strict";
 
 var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
 var rename = require("gulp-rename");
 
 gulp.task("css", function () {
   return gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(rename("style.css"))
     .pipe(gulp.dest("css"))
 });
 
-gulp.task("start", gulp.series("css"));
+gulp.task("watch", function () {
+  gulp.watch("less/**/*.less").on("change", gulp.series("css"));
+});
+
+gulp.task("start", gulp.series("css", "watch"));
